e5f2401cd6bb9eb0c4fd5a75de6059ffd7a1bde9
CsarChvz <cesarchavezrodriguez9@gmail.com>
2022-07-26 14:11:23 -0500

    UserUid with expo secure

--- app/utils/session.server.js
+++ app/utils/session.server.js
@@ -1,13 +1,12 @@
 import * as SecureStore from "expo-secure-store";
 import { getSessionToken } from "./auth.server";
 
+
+{/* Token -- THINGS*/}
+
 async function createUserSession(idToken) {
   const token = await getSessionToken(idToken);
   await SecureStore.setItemAsync("sessionToken", token);
-}
-
-async function setUserUid(uid) {
-  await SecureStore.setItemAsync("uid", uid);
 }
 
 async function getUserSession() {
@@ -19,4 +18,18 @@ async function deleteUserSession() {
   await SecureStore.deleteItemAsync("sessionToken");
 }
 
-export { createUserSession, getUserSession, deleteUserSession };
+
+{/* Expo Secure -- UID */}
+
+async function getUserUid() {
+  const uid = await SecureStore.getItemAsync("uid");
+  return uid;
+}
+
+async function setUserUid(uid) {
+  await SecureStore.setItemAsync("uid", uid);
+}
+async function deleteUserUid() {
+  await SecureStore.deleteItemAsync("uid");
+}
+export { createUserSession, getUserSession, deleteUserSession, getUserUid, deleteUserUid };
